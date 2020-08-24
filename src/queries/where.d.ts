972c0d67621bd9e19bddba6be29442a4fe7eff8b
Pierre teyssedre <pierre@teyssedre.ca>
2020-08-24 10:54:00 -0400

    updating dependencies

---
+++ src/queries/where.d.ts
@@ -0,0 +1,10 @@
+import { WhereOperator } from "./query";
+export declare function Where(clause: any, operator?: WhereOperator): WhereClause;
+export declare class WhereClause {
+    static fromModel(clause: any, operator?: WhereOperator): WhereClause;
+    sql: string;
+    constructor(model?: any, operator?: WhereOperator);
+    and(field: string | any, operator?: WhereOperator, value?: number | string): WhereClause;
+    or(field: string | any, operator?: WhereOperator, value?: number | string): WhereClause;
+    private parse(model, operator?);
+}
---
+++ src/queries/where.js
@@ -0,0 +1,59 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const query_1 = require("./query");
+function Where(clause, operator = query_1.WhereOperator.AND) {
+    return WhereClause.fromModel(clause, operator);
+}
+exports.Where = Where;
+class WhereClause {
+    constructor(model, operator = query_1.WhereOperator.AND) {
+        this.sql = "";
+        this.sql = "";
+        this.parse(model, operator);
+    }
+    static fromModel(clause, operator = query_1.WhereOperator.AND) {
+        return new WhereClause(clause, operator);
+    }
+    and(field, operator, value) {
+        if (value && typeof value === "string") {
+            value = `'${value}'`;
+        }
+        this.sql = this.sql.trim() + " AND " +
+            `${field} ${operator ? operator.toString() : value} ${value ? value : ""}`.trim();
+        return this;
+    }
+    or(field, operator, value) {
+        if (value && typeof value === "string") {
+            value = `'${value}'`;
+        }
+        this.sql = this.sql.trim() + " OR " +
+            `${field} ${operator ? operator.toString() : value} ${value ? value : ""}`.trim();
+        return this;
+    }
+    parse(model, operator = query_1.WhereOperator.AND) {
+        const keys = Object.keys(model);
+        for (let i = 0; i < keys.length; i++) {
+            const v = model[keys[i]];
+            const str = typeof v === "string" ? `'${v}'` : `${v}`;
+            const n = i + 1 < keys.length ? " " + operator : "";
+            const s = i === 0 ? "" : " ";
+            switch (v) {
+                case null:
+                    this.sql += `${s}${keys[i]} IS NULL${n}`;
+                    break;
+                case "null":
+                case "NULL":
+                case "NOT NULL":
+                case "not null":
+                    this.sql += `${s}${keys[i]} IS ${v.toUpperCase()}${n}`;
+                    break;
+                default:
+                    this.sql += `${s}${keys[i]} = ${str}${n}`;
+                    break;
+            }
+        }
+        return this;
+    }
+}
+exports.WhereClause = WhereClause;
+//# sourceMappingURL=where.js.map
